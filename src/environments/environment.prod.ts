ab47d1cd669c17dbbbe867d2d41bf3c326311c3a
nourelhouda-taroudi <nourelhoudataroudi@gmail.com>
2023-01-10 02:18:55 +0100

    added github actions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   BASE_URL_BACKOFFICE:'backoffice-back-link',
   KC_URL:'http://ec2-44-202-143-82.compute-1.amazonaws.com:8089/',
   KC_REALM:'EDB_TRANSFERS',
-  KC_CLIENT_ID:'backoffice'
+  KC_CLIENT_ID:'backoffice',
+  BASE_URL_BACKOFFICE_SETTINGS:'http://localhost:3000/api/v1'
 };
